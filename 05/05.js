396dbcb153114c2ebf9b8994a48774d088edeefa
YusukeE0330 <YusukeEmbutsu@net80-dhcp-82.sfc.keio.ac.jp>
2016-06-09 11:54:59 +0900

    修正２

--- 05/05.js
+++ 05/05.js
@@ -1,15 +1,6 @@
 function strlen(a){
-var text_a = "Hello, ";
-var text_b = "world!";
-
-var length_a = strlen(text_a);
-length_a == 7; // true
-
-var length_b = strlen(text_b);
-length_b == 6; // true
-
-var new_text = concat(text_a, text_b); // "Hello, world!"
-var length = strlen(new_text);
-length == 13;
+    return a.length
 }
-      
+function concat(b){
+
+} 
