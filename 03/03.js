095f48b2473a175d736e7902a72e1bd660a0f9f6
YusukeE0330 <YusukeEmbutsu@net80-dhcp-82.sfc.keio.ac.jp>
2016-06-09 11:21:18 +0900

    修正１０

--- 03/03.js
+++ 03/03.js
@@ -1,6 +1,6 @@
 function isEvenNumber(a){
-    var result = a % 2 == 0 == true;
-    var result = !true == false;
-
-console.log(result);
+	if( a % 2 == 0 )
+		return true;
+	else
+		return false;
 }
